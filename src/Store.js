ec640e2053308c042bcce9cad59f167c7f8c1e82
Sebastian Arroyo <larroyo@fi.uba.ar>
2021-09-21 14:46:39 -0300

    Added Basic Styling to rendered items and product details

--- src/Store.js
+++ src/Store.js
@@ -24,45 +24,59 @@ export class Store {
         let products = [
             {
             id: 1,
-            image: '',
-            name: 'Alienware m15',
+            image: 'resources/GeForce-RTX-3090.jpg',
+            name: 'Nvidia GeForce RTX 3090',
             stock: '7',
-            price: '999.99'
+            price: '$1,499'
             },
             {
             id: 2,
-            image: 'assets/img/alienware3.png',
-            name: 'Alienware R4',
+            image: 'resources/AMD-Radeon-RX-6900-XT.jpg',
+            name: 'AMD Radeon RX 6900 XT',
             stock: '3',
-            price: '1399.89'
+            price: '999'
             },
             {
             id: 3,
-            image: 'assets/img/alienware2.png',
-            name: 'Alienware 17',
+            image: 'resources/Nvidia-RTX-3060-12GB.jpg',
+            name: 'Nvidia GeForce RTX 3060 Ti',
             stock: '0',
-            price: '1299.89'
+            price: '399'
             },
             {
             id: 4,
-            image: 'assets/img/alienware2.png',
-            name: 'Alienware 17',
+            image: 'resources/AMD-Radeon-RX-5500-XT.jpg',
+            name: 'AMD Radeon RX 5500 XT',
             stock: '11',
-            price: '1299.89'
+            price: '169'
             },
             {
             id: 5,
-            image: 'assets/img/alienware2.png',
-            name: 'Alienware 17',
+            image: 'resources/Nvidia-Geforce-Gtx-1650.jpg',
+            name: 'Nvidia GeForce GTX 1650 Super',
             stock: '8',
-            price: '1299.89'
+            price: '159'
             },
             {
             id: 6,
-            image: 'assets/img/alienware2.png',
-            name: 'Alienware 17',
+            image: 'resources/Nvidia-Geforce-RTX-3080.png',
+            name: 'Nvidia GeForce RTX 3080',
             stock: '2',
-            price: '1299.89'
+            price: '699'
+            },
+            {
+            id: 7,
+            image: 'resources/AMD-Radeon-RX-5700.jpg',
+            name: 'AMD Radeon RX 5700',
+            stock: '2',
+            price: '380'
+            },
+            {
+            id: 8,
+            image: 'resources/Nvidia-Geforce-GT-710-2GB.jpg',
+            name: 'Nvidia Geforce GT 710 2GB',
+            stock: '2',
+            price: '60'
             },
         ]
         products.forEach((p) => {
